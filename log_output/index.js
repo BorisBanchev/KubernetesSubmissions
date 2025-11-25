f8543d99408aac2758b9b42447f659d7bc7576a7
BorisBanchev <boris.banchev03@gmail.com>
2025-11-25 16:35:45 +0200

    Add exercise 1.7

--- log_output/index.js
+++ log_output/index.js
@@ -1,3 +1,9 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+
 const characters =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
@@ -15,3 +21,22 @@ const generateString = () => {
 };
 
 generateString();
+
+const generateStringAndTimestamp = () => {
+  let result = " ";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  const timestamp = new Date().toISOString();
+  const timestampAndString = `String:${result}, created at: ${timestamp}`;
+  return timestampAndString;
+};
+
+app.get("/", (request, response) => {
+  response.send(generateStringAndTimestamp());
+});
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
